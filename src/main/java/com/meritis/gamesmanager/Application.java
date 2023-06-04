48abb776cf12c67aebec1369faf060a449fb5ad2
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-04 19:54:59 +0200

    Create tournament at runtime

--- src/main/java/com/meritis/gamesmanager/Application.java
+++ src/main/java/com/meritis/gamesmanager/Application.java
@@ -1,14 +1,14 @@
 package com.meritis.gamesmanager;
 
+import com.meritis.gamesmanager.model.request.TournamentRequest;
 import com.meritis.gamesmanager.service.TournamentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.IntStream;;
 
 @SpringBootApplication
 public class Application {
@@ -17,8 +17,12 @@ public class Application {
 		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
 		TournamentService tournamentService = applicationContext.getBean(TournamentService.class);
-
-		List<Integer> teamInfoIds = IntStream.rangeClosed(1, 16).boxed().collect(Collectors.toCollection(ArrayList::new));
-
+		TournamentRequest tournamentRequest = new TournamentRequest();
+		tournamentRequest.setName("First tournament");
+		tournamentRequest.setNbOfGroups(4);
+		tournamentRequest.setTeamIds(IntStream.rangeClosed(1, 16).mapToObj(Long::valueOf).collect(Collectors.toList()));
+		tournamentRequest.setStartDate(LocalDate.now());
+		tournamentRequest.setDirectElimination(false);
+		tournamentService.createTournament(tournamentRequest);
 	}
 }
